95e51669d8da3de829d07bc31127f3640924d364
amr anwar <3mr3nwar@gmail.com>
2025-11-16 07:24:17 +0200

    Suspense SSR

--- app/(dashboard)/dashboard/layout.js
+++ app/(dashboard)/dashboard/layout.js
@@ -1,13 +1,20 @@
 import "@/app/globals.css"
+import { Suspense } from "react";
 
 export default function ContentLayout({ children, users, tasks }) {
     return (
         <div className="space-y-4">
             <div>{children}</div>
+            <Suspense fallback={
+                <div className="min-h-screen flex justify-center items-center">
+                <h1>Loading your content</h1>
+                </div>
+                }>
             <div className="flex flex-col lg:flex-row gap-4">
                 <div className="w-full lg:flex-1 lg:max-w-[50%]">{users}</div>
                 <div className="w-full lg:flex-1 lg:max-w-[50%]">{tasks}</div>
             </div>
+            </Suspense>
         </div>
     );
 }
